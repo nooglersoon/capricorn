3a9ccb7c9002ec19bf2ea95d15f45e24ead9f084
nooglersoon <nooglersoon@outlook.co.id>
2024-01-04 14:06:57 +0700

    Initial Commit

---
+++ Capricorn/CapricornApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapricornApp.swift
+//  Capricorn
+//
+//  Created by fauzia076860 on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CapricornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capricorn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Capricorn
+//
+//  Created by fauzia076860 on 04/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
