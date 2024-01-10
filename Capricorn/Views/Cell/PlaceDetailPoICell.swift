9ef2d44aca64a4b1b6934c10ccce7db16805c710
nooglersoon <nooglersoon@outlook.co.id>
2024-01-10 11:14:22 +0700

    add linear gradient layer

--- Capricorn/Views/Cell/PlaceDetailPoICell.swift
+++ Capricorn/Views/Cell/PlaceDetailPoICell.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct PlaceDetailPoICell: View {
     let index: Int
-
+    
     var body: some View {
         ZStack (alignment: .bottomTrailing) {
             Image("photo")
@@ -17,12 +17,22 @@ struct PlaceDetailPoICell: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 160, height: 100)
                 .clipped()
-
+            
+            LinearGradient(
+                gradient: Gradient(
+                    colors: [Color.black.opacity(1.0), Color.black.opacity(0.0)]),
+                startPoint: .bottom,
+                endPoint: .top
+            )
+            .frame(width: 160, height: 100)
+            .clipped()
+            
             Text("Item \(index)")
                 .foregroundColor(.white)
                 .bold()
                 .font(.caption)
                 .padding(8)
+            
         }
         .cornerRadius(8)
     }
